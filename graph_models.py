791d59cd803005341b954aa879db9855f789ebb3
Abdulmuizz Abdulrazzak Khalak <58474622+muizzkhalak@users.noreply.github.com>
2024-12-26 14:04:21 +0100

    Update graph_models.py

--- graph_models.py
+++ graph_models.py
@@ -185,14 +185,18 @@ class HECO(CasePreprocessing):
         available_feature_legislation = list(self.feature_legislations.keys())
         missing_legislation_features = list(set(legislation_nodes_hg) - set(available_feature_legislation))
         missing_legislation_features = [self.legislations[leg] for leg in missing_legislation_features]
+        print(hg.num_nodes('legislation'))
         hg = dgl.remove_nodes(hg, torch.tensor(missing_legislation_features), ntype='legislation')
+        print(hg.num_nodes('legislation'))
 
         # Remove cases without any features
         case_nodes_hg = [self._key_from_value(self.rev_cases,i) for i in hg.nodes('case').tolist()]
         available_feature_case = list(self.feature_cases.keys())
         missing_case_features = list(set(case_nodes_hg) - set(available_feature_case))
         missing_case_features = [self.cases[cas] for cas in missing_case_features]
+        print(hg.num_nodes('case'))
         hg = dgl.remove_nodes(hg, torch.tensor(missing_case_features), ntype='case')
+        print(hg.num_nodes('case'))
 
         hg = hg.to(self.device)
 
